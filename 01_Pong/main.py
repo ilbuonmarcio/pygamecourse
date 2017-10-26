1a62150eb1f2d9e8707d90ea76b11a8054888354
mrcz <mrcz@bigcodelover.lan>
2017-10-26 20:10:20 +0200

    Added initial pong development

--- 01_Pong/main.py
+++ 01_Pong/main.py
@@ -5,19 +5,33 @@ pygame.init()
 
 GAME_RES = WIDTH, HEIGHT = 800, 600
 FPS = 60
-GAME_TITLE = 'Default Game Title'
+GAME_TITLE = 'Pong - MarconiGames'
 
 window = pygame.display.set_mode(GAME_RES, HWACCEL|HWSURFACE|DOUBLEBUF)
 pygame.display.set_caption(GAME_TITLE)
+pygame.key.set_repeat(10,10)
 clock = pygame.time.Clock()
 
 # Game Values
 
+player_sprite = pygame.image.load('./images/player_sprite.png')
+player_rect = player_sprite.get_rect()
 player_x = 20
 player_y = 20
-player_x_speed = 10
-player_y_speed = 10
-player_sprite = pygame.image.load('./images/player_sprite.png')
+
+enemy_sprite = pygame.image.load('./images/enemy_sprite.png')
+enemy_rect = enemy_sprite.get_rect()
+enemy_x = WIDTH - 20 - enemy_rect.width
+enemy_y = 20
+
+bar_speed = player_y_speed = enemy_y_speed = 15
+
+ball_sprite = pygame.image.load('./images/ball_sprite.png')
+ball_rect = ball_sprite.get_rect()
+ball_x = WIDTH // 2 - ball_rect.width // 2
+ball_y = HEIGHT // 2 - ball_rect.height // 2
+ball_x_speed = 6
+ball_y_speed = 6
 
 background_color = (200, 200, 200)
 
@@ -27,8 +41,10 @@ background_color = (200, 200, 200)
 game_ended = False
 while not game_ended:
 
-    # Event handling
+    ##### Event handling
     for event in pygame.event.get():
+
+        # Handling keys for closing the game
         if event.type == QUIT:
             game_ended  = True
             break
@@ -37,22 +53,51 @@ while not game_ended:
                 game_ended  = True
                 break
 
-    # Game logic
-    if player_x >= WIDTH-256 or player_x <= 0:
-        player_x_speed *= -1
+    # Player's Input Handling
+    keys_pressed = pygame.key.get_pressed()
 
-    if player_y >= HEIGHT-256 or player_y <= 0:
-        player_y_speed *= -1
+    # Player Input
+    if keys_pressed[K_w]:
+        player_y -= bar_speed
+    if keys_pressed[K_s]:
+        player_y += bar_speed
+
+    # Enemy Input
+    if keys_pressed[K_o]:
+        enemy_y -= bar_speed
+    if keys_pressed[K_k]:
+        enemy_y += bar_speed
+
+    ##### Game logic
+    # Keeping ball into screen
+    if ball_x + ball_rect.width >= WIDTH or ball_x <= 0:
+        ball_x_speed *= -1
+    if ball_y + ball_rect.height >= HEIGHT or ball_y <= 0:
+        ball_y_speed *= -1
+
+    # Keeping bars into screen
+    if player_y + player_rect.height >= HEIGHT - 20:
+        player_y = HEIGHT - player_rect.height - 20
+    if player_y < 20:
+        player_y = 20
+
+    if enemy_y + enemy_rect.height >= HEIGHT - 20:
+        enemy_y = HEIGHT - enemy_rect.height - 20
+    if enemy_y < 20:
+        enemy_y = 20
+
+    # Moving bar
+    ball_x += ball_x_speed
+    ball_y += ball_y_speed
 
 
-    player_x += player_x_speed
-    player_y += player_y_speed
-
-
-    # Display update
+    ##### Display Drawing
     pygame.Surface.fill(window, background_color)
     pygame.Surface.blit(window, player_sprite, (player_x, player_y))
+    pygame.Surface.blit(window, enemy_sprite, (enemy_x, enemy_y))
+    pygame.Surface.blit(window, ball_sprite, (ball_x, ball_y))
 
+    ##### Display Update
     pygame.display.update()
     clock.tick(FPS)
 
